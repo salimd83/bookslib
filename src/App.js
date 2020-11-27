ee04d042a130e4e66a4720745f6b2c9322992031
Salim Dirani <salimdirani@gmail.com>
2020-11-27 16:41:38 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const [book, setBook] = useState({
+    title: '',
+    pages: '',
+    publishDate: ''
+  });
+
+  const onChange = (e) => {
+    setBook({
+      ...book,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(book);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Books library</h1>
+
+      <form onSubmit={onSubmit}>
+        <div>
+          <label htmlFor="book-title">Title:</label>
+          <input type="text" value={book.title} onChange={onChange} name="title" id="book-title" />
+        </div>
+        <div>
+          <label htmlFor="book-pages">Number of pages:</label>
+          <input type="number" value={book.pages} onChange={onChange} name="pages" id="book-pages" />
+        </div>
+        <div>
+          <label htmlFor="book-publish-date">Date of publishing:</label>
+          <input type="date" value={book.publishDate} onChange={onChange} name="publishDate" id="book-publish-date" />
+        </div>
+        <div>
+          <button type="submit">Save</button>
+        </div>
+      </form>
     </div>
   );
 }
